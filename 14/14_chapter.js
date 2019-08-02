7cfac63c5fb13cebd7262c331e70944a6a9f9f73
fireyw <fireyw@naver.com>
2019-08-02 18:49:15 +0900

    chapter14

--- 14/14_chapter.js
+++ 14/14_chapter.js
@@ -1,2 +1,12 @@
-const fs= require('fs');
+function countdown(seconds){
+    return new Promise(function(resolve, reject){
+        for(let i =seconds; i>=0; i--){
+            setTimeout(function(){
+                if(i>0) console.log(i+ '...')
+                else resolve(console.log('GO!'));
+            }, (seconds-i)*1000);
+        }
+    })
+}
 
+countdown(10);
